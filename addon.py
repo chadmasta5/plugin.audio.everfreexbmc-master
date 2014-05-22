36383974e541d3036935419d4a24c316526ac9c7
chadmasta5 <chadmasta5@gmail.com>
2014-05-22 16:48:00 -0500

    Update addon.py

--- addon.py
+++ addon.py
@@ -5,7 +5,8 @@ import xbmcplugin
 addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'audio')
-
+#li.setIconImage('http://i59.tinypic.com/2wf92z6.jpg')
+#li.setProperty('fanart_image', 'http://i59.tinypic.com/b669vt.jpg')
 url = 'http://chadmasta5.weebly.com/uploads/6/0/1/9/6019696/listen.pls'
 li = xbmcgui.ListItem('Listen', iconImage='http://i60.tinypic.com/2hnozec.png')
 li.setProperty('fanart_image', 'http://i59.tinypic.com/b669vt.jpg')
