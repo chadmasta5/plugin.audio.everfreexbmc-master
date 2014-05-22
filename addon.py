d32c578dd8800f74894b9454fcec48f9d96095b7
chadmasta5 <chadmasta5@gmail.com>
2014-05-22 15:56:02 -0500

    Update addon.py

--- addon.py
+++ addon.py
@@ -6,8 +6,11 @@ addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'audio')
 
+li.setIconImage('http://i59.tinypic.com/2wf92z6.png')
+li.setProperty('fanart_image', 'http://fc07.deviantart.net/fs71/i/2013/251/b/2/the_everfree_network_by_kibbiethegreat-d6lk9hi.png')
 url = 'http://chadmasta5.weebly.com/uploads/6/0/1/9/6019696/listen.pls'
-li = xbmcgui.ListItem('Listen', iconImage='http://i59.tinypic.com/2wf92z6.png')
+li = xbmcgui.ListItem('Listen', iconImage='http://i60.tinypic.com/2hnozec.png')
+li.setProperty('fanart_image', 'http://fc07.deviantart.net/fs71/i/2013/251/b/2/the_everfree_network_by_kibbiethegreat-d6lk9hi.png')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
 xbmcplugin.endOfDirectory(addon_handle)
