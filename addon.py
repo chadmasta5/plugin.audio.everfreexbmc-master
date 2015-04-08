7f43a78a5f2ba89f5e416b3d08f372fb25acc9d7
chadmasta5 <chadmasta5@gmail.com>
2015-04-08 17:13:19 -0500

    Update addon.py
    fixed radio source

--- addon.py
+++ addon.py
@@ -6,7 +6,7 @@ addon_handle = int(sys.argv[1])
 
 xbmcplugin.setContent(addon_handle, 'audio')
 
-url = 'http://chadmasta5.weebly.com/uploads/6/0/1/9/6019696/listen.pls'
+url = 'http://chadmasta5.weebly.com/uploads/6/0/1/9/6019696/everfree_radio.strm'
 li = xbmcgui.ListItem('Listen', iconImage='http://i60.tinypic.com/2hnozec.png')
 li.setProperty('fanart_image', 'http://i59.tinypic.com/b669vt.jpg')
 xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
